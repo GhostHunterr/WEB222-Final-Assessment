dd0f11aed92d74191ff3dfbd7aa5d7b7b7685845
Farhan Sarang <farhansarang7@gmail.com>
2023-04-21 14:32:58 -0400

    Fixed the submit button message

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   var bttnEle = document.getElementById("contact-btn");
   var formEle = document.getElementById("contact-form-id");
-  var submitEle = document.getElementById("submit-btn");
 
   bttnEle.addEventListener("click", function () {
     window.scrollTo({ top: formEle.offsetTop, behavior: "smooth" });
-  });
-
-  submitEle.addEventListener("click", () => {
-    alert("Thank you for Reaching Me out\n I will get back to you soon.");
   });
 });
